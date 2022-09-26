3d82b9624bce7c1bd23066d123c828a87c492111
bhanusanghi <bhanussanghi@gmail.com>
2022-09-26 19:27:20 +0530

    Adds networks to hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,42 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+
+
+
+  networks: {
+    mainnet: {
+      timeout: 60000,
+      chainId: 1,
+      url: process.env.RPC_URL || `https://mainnet.infura.io/v3/${process.env.MAINNET_KEY}`,
+    },
+    hardhat: {
+      allowUnlimitedContractSize: true,
+      initialBaseFeePerGas: 0,
+      chainId: 1337,
+      forking:
+      {
+        enabled: true,
+        url: process.env.OPTIMISM_MAINNET_KEY || '',
+        // blockNumber: number,
+      }
+    },
+
+    'optimism': {
+      url: "https://mainnet.optimism.io",
+      // accounts: [privateKey1, privateKey2, ...]
+    },
+    // for testnet
+    'optimism-goerli': {
+      url: "https://goerli.optimism.io",
+      // accounts: [privateKey1, privateKey2, ...]
+    },
+    // for the local dev environment
+    'optimism-local': {
+      url: "http://localhost:8545",
+      // accounts: [privateKey1, privateKey2, ...]
+    },
+  }
 };
 
 export default config;
