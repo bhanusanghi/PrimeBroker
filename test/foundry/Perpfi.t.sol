0d5af54306757c74746f3fe70cdf67668bc53e56
0xAshish <a4ashishrp@gmail.com>
2023-02-21 15:53:59 +0400

    remove out

---
+++ test/foundry/Perpfi.t.sol
@@ -0,0 +1,206 @@
+pragma solidity ^0.8.10;
+
+import {BaseSetup} from "./BaseSetup.sol";
+import {Utils} from "./utils/Utils.sol";
+import "forge-std/Test.sol";
+import "forge-std/console2.sol";
+
+import {IAddressResolver} from "../../contracts/Interfaces/SNX/IAddressResolver.sol";
+import {IFuturesMarketManager} from "../../contracts/Interfaces/SNX/IFuturesMarketManager.sol";
+import {IFuturesMarket} from "../../contracts/Interfaces/SNX/IFuturesMarket.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import {SignedMath} from "@openzeppelin/contracts/utils/math/SignedMath.sol";
+import {SafeCastUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/math/SafeCastUpgradeable.sol";
+import {SettlementTokenMath} from "../../contracts/Libraries/SettlementTokenMath.sol";
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+/**
+ * setup
+ * Open position
+ * margin and leverage min max fuzzy
+ * fee
+ * update
+ * multiple markets
+ * liquidate perpfi
+ * liquidate on GB
+ * close positions
+ * pnl
+ * pnl with ranges and multiple positions
+ */
+contract Perpfitest is BaseSetup {
+     using SafeMath for uint256;
+    using Math for uint256;
+    using SettlementTokenMath for uint256;
+    using SettlementTokenMath for int256;
+    using SafeCastUpgradeable for uint256;
+    using SafeCastUpgradeable for int256;
+    using SignedMath for int256;
+
+    uint256 constant ONE_USDC = 10**6;
+    int256 constant ONE_USDC_INT = 10**6;
+    uint256 largeAmount = 1_000_000 * ONE_USDC;
+    bytes32 snxUni_marketKey = bytes32("sUNI");
+    bytes32 snxEth_marketKey = bytes32("sETH");
+
+    bytes32 perpAaveKey = keccak256("PERP.AAVE");
+    bytes32 invalidKey = keccak256("BKL.MKC");
+    bytes32 snxUniKey = keccak256("SNX.UNI");
+    bytes32 snxEthKey = keccak256("SNX.ETH");
+
+    address bobMarginAccount;
+    address aliceMarginAccount;
+
+    address uniFuturesMarket;
+
+    address ethFuturesMarket;
+    address perpAaveMarket;
+   function setUp() public {
+        uint256 forkId = vm.createFork(
+            vm.envString("ARCHIVE_NODE_URL_L2"),
+            69164900
+        );
+        vm.selectFork(forkId);
+        utils = new Utils();
+        setupUsers();
+        setupContractRegistry();
+        setupPriceOracle();
+        setupMarketManager();
+        setupMarginManager();
+        setupRiskManager();
+        setupCollateralManager();
+        setupVault();
+
+        riskManager.setCollateralManager(address(collateralManager));
+        riskManager.setVault(address(vault));
+
+        marginManager.setVault(address(vault));
+        marginManager.SetRiskManager(address(riskManager));
+
+        setupProtocolRiskManagers();
+
+        // collaterals.push(usdc);
+        // collaterals.push(susd);
+        collateralManager.addAllowedCollateral(usdc, 100);
+        collateralManager.addAllowedCollateral(susd, 100);
+        //fetch snx market addresses.
+        snxFuturesMarketManager = IAddressResolver(SNX_ADDRESS_RESOLVER)
+            .getAddress(bytes32("FuturesMarketManager"));
+        uniFuturesMarket = IFuturesMarketManager(snxFuturesMarketManager)
+            .marketForKey(snxUni_marketKey);
+        vm.label(uniFuturesMarket, "UNI futures Market");
+        ethFuturesMarket = IFuturesMarketManager(snxFuturesMarketManager)
+            .marketForKey(snxEth_marketKey);
+        // ethPerpsV2Market = 0x35CcAC0A67D2a1EF1FDa8898AEcf1415FE6cf94c;
+        vm.label(ethFuturesMarket, "ETH futures Market");
+
+        marketManager.addMarket(
+            snxUniKey,
+            uniFuturesMarket,
+            address(snxRiskManager)
+        );
+        // marketManager.addMarket(
+        //     snxEthKey,
+        //     ethFuturesMarket,
+        //     address(snxRiskManager)
+        // );
+        marketManager.addMarket(
+            perpAaveKey,
+            perpClearingHouse,
+            address(perpfiRiskManager)
+        );
+        snxRiskManager.toggleAddressWhitelisting(uniFuturesMarket, true);
+        snxRiskManager.toggleAddressWhitelisting(ethFuturesMarket, true);
+        uint256 usdcWhaleContractBal = IERC20(usdc).balanceOf(
+            usdcWhaleContract
+        );
+        vm.startPrank(usdcWhaleContract);
+        IERC20(usdc).transfer(admin, largeAmount * 2);
+        IERC20(usdc).transfer(bob, largeAmount);
+        // IERC20(usdc).transfer(alice, largeAmount);
+        // IERC20(usdc).transfer(charlie, largeAmount);
+        // IERC20(usdc).transfer(david, largeAmount);
+        vm.stopPrank();
+
+        uint256 adminBal = IERC20(usdc).balanceOf(admin);
+        // fund usdc vault.
+        vm.startPrank(admin);
+        IERC20(usdc).approve(address(vault), largeAmount);
+        vault.deposit(largeAmount, admin);
+        vm.stopPrank();
+
+        // setup and fund margin accounts.
+        vm.prank(bob);
+        bobMarginAccount = marginManager.openMarginAccount();
+        vm.prank(alice);
+        aliceMarginAccount = marginManager.openMarginAccount();
+
+        // assume usdc and susd value to be 1
+        uint80 roundId = 18446744073709552872;
+        int256 answer = 100000000;
+        uint256 startedAt = 1674660973;
+        uint256 updatedAt = 1674660973;
+        uint80 answeredInRound = 18446744073709552872;
+        vm.mockCall(
+            sUsdPriceFeed,
+            abi.encodeWithSelector(
+                AggregatorV3Interface.latestRoundData.selector
+            ),
+            abi.encode(roundId, answer, startedAt, updatedAt, answeredInRound)
+        );
+        vm.mockCall(
+            usdcPriceFeed,
+            abi.encodeWithSelector(
+                AggregatorV3Interface.latestRoundData.selector
+            ),
+            abi.encode(roundId, answer, startedAt, updatedAt, answeredInRound)
+        );
+    }
+    // Internal 
+    function testOpenPositionPerp() public {
+        uint256 liquiMargin = 100_000 * ONE_USDC;
+        assertEq(vault.expectedLiquidity(), largeAmount);
+        vm.startPrank(bob);
+        IERC20(usdc).approve(bobMarginAccount, liquiMargin);
+        vm.expectEmit(true, true, true, false, address(collateralManager));
+        emit CollateralAdded(bobMarginAccount, usdc, liquiMargin, 0);
+        collateralManager.addCollateral(usdc, liquiMargin);
+        console.log(bobMarginAccount,'bob margin account in test',
+        IERC20(usdc).balanceOf(bob),collateralManager.totalCollateralValue(bobMarginAccount));
+        // vm.assume(
+        //     marginSNX1 > 1000 ether && marginSNX1 < 100_000 ether // otherwise the uniswap swap is extra bad
+        // );
+        address[] memory destinations = new address[](2);
+        bytes[] memory data1 = new bytes[](2);
+        // destinations[0] = 0x00;
+        
+        data1[0] = abi.encodeWithSignature(
+            "approve(address,uint256)",
+            address(perpClearingHouse),
+            liquiMargin  
+        );
+        // iface.encodeFunctionData("openPosition", [baseToken, isBaseToQuote, isExactInput, oppositeAmountBound, amount, sqrtPriceLimitX96, deadline, referralCode])
+        // data1[1] = abi.encodeWithSignature(
+        //     "openPosition(address,uint256)",
+        //     int256,
+        //     bytes32  
+        // );
+        // vm.expectEmit(true, true, true, true, address(marginManager));
+        // emit MarginTransferred(
+        //     bobMarginAccount,
+        //     uniFuturesMarket,
+        //     susd,
+        //     int256(marginSNX1),
+        //     int256(marginSNX1).convertTokenDecimals(18, 6)
+        // );
+        // vm.expectEmit(true, true, false, true, address(susd));
+        // emit Transfer(bobMarginAccount, address(0x00), marginSNX1);
+        // vm.expectEmit(true, false, false, true, address(susd));
+        // emit Burned(bobMarginAccount, marginSNX1);
+        // vm.expectEmit(true, false, false, true, address(uniFuturesMarket));
+        // emit MarginTransferred(bobMarginAccount, int256(marginSNX1));
+
+        // marginManager.openPosition(snxUniKey, destinations, data1);
+    }
+}
